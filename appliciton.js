f3a5648aec4973f5688e03ca591b6f4532458e3b
hridaysonar <mdhridaysonar@gmail.com>
2025-03-02 01:59:54 +0600

    aside bar and all code modify

--- appliciton.js
+++ appliciton.js
@@ -3,9 +3,23 @@ document.getElementById("back-now").addEventListener("click", function () {
   });
 
 //   colorjavasript
-  document.getElementById('thime-change').addEventListener('click', function() {
-    const colors = ['#FF5733', '#33FF57', '#3357FF', '#FF33A6', '#A633FF','#FFA07A','#800080','#FF00FF'];
+  document.getElementById('thimeChange').addEventListener('click', function() {
+    const colors = ['#FF5733', '#33FF57', '#3357FF', '#FF33A6', '#A633FF','#FFA07A','#800080'];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     document.body.style.backgroundColor = randomColor;
 });
+
+ 
+// date
+document.getElementById("new-date").innerText = new Date().toDateString();
+function time() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
+  }
+  
+//   date done
+
 
